29a66487266942994bafb54b0ef4b6c8580f2fa6
ADS\vazquezj <vazquezj@indiana.edu>
2017-11-09 13:06:09 -0500

    11-9-17 Losing Condition working.

--- Cracker.gd
+++ Cracker.gd
@@ -1,4 +1,16 @@
 extends Area2D
 
+var lose = false
+
 func _on_GrahamCracker_area_entered( area ):
-	
+	#print (area.is_in_group("Creep"))
+	#print (area.get_node("MarshmallowFaces").frame)
+	if area.is_in_group("Creep") and area.get_node("MarshmallowFaces").frame == 0:
+		#print("Losing?")
+		$".".lose_condition()
+func lose_condition():
+	#print ("Lose")
+	if lose == false:
+		lose = true
+		$"../LoseScreen".position = $"../Player".global_position
+		$"../LoseScreen".visible = true
